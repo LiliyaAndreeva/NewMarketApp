6affd8964faba44edd622f5e1bf13fbfc47b96fb
tatanadubova <tatyana.dubova.1986@gmail.com>
2023-10-30 14:54:53 +0300

    add login logic to the application + design LaunchScreen and nextButton

--- NewMarketApp/ViewControllers/LoginViewController.swift
+++ NewMarketApp/ViewControllers/LoginViewController.swift
@@ -8,17 +8,48 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var nextButton: UIButton!
+    
+    private let user = User.getUser()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        nextButton.layer.cornerRadius = 50
-        
-       
+        userNameTextField.text = user.username
+        passwordTextField.text = user.password
+        print("User ID: ", user.userId)
+        nextButton.clipsToBounds = true
     }
     
-
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        nextButton.layer.cornerRadius = nextButton.bounds.height / 2
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    @IBAction func nextButtonPressed() {
+        guard userNameTextField.text == user.username,
+                passwordTextField.text == user.password else {
+            showAlert(
+                title: "Invalid login or password",
+                message: "Please, enter correct login and password",
+                textField: passwordTextField
+            )
+            return
+        }
+    }
+    
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
 }
